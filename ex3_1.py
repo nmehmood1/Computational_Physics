2854308268194ab6630b708d87c66feb04c67d8c
nmehmood1 <32763956+nmehmood1@users.noreply.github.com>
2018-05-13 16:54:10 +0100

    Add files via upload

---
+++ ex3_1.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+r = 5
+#plot first 1000 data points of sunspot count vs month
+
+sunspots = np.loadtxt("sunspots.txt",float)
+xData = sunspots[:,0]
+yData = sunspots[:,1]
+
+plt.plot(xData,yData)
+plt.xlabel('Months Since Jan 1749')
+plt.ylabel('Number of Sunspots')
+plt.xlim(0,1000)
+
+#plot running average on same graph
+runSum = np.cumsum(yData) # I have chosen to produce an array using cumsum and 
+#                           divide elementwise with a numberline array
+numLine = np.arange(1,3144)
+runAvg = (np.divide(runSum,numLine))
+
+plt.plot(xData,runAvg,label = 'Running Avg.')
+plt.xlim(0,1000)
+plt.legend()
+plt.savefig('ex3.1.png')
